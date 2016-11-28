2d5ad25640e2b5fea6000da92ff429be40cda043
AlanJager <873863981@qq.com>
2016-11-28 22:37:22 +0800

    make user to class

--- twitter.rb
+++ twitter.rb
@@ -2,6 +2,7 @@ require 'mechanize'
 require 'json'
 require 'base64'
 require 'yaml'
+require_relative 'request'
 require_relative 'auth'
 require_relative 'user'
 require_relative 'utils'
@@ -14,5 +15,9 @@ token = auth(agent)
 # request_timeline(agent, token)
 
 # request user
-# request_user(agent, token, anju_inami)
+# request_user(agent, token, 'anju_inami')
+
+# request user following
+user = User.new(agent, token)
+p user.friend_ids('anju_inami')
 
--- user.rb
+++ user.rb
@@ -1,13 +1,58 @@
-def request_user(agent, token, screen_name)
-  url = 'https://api.twitter.com/1.1/users/show/' + screen_name + '.json'
-  settings = YAML.load_file('settings.yml')
+class User
+  attr_reader :agent
+  attr_reader :token
 
-  data = {
-    'Host' => 'api.twitter.com',
-    'User-Agent' => settings['user_agent'],
-    'Authorization' => "Bearer " + token,
-    'Accept-Encoding' => 'gzip'
-  }
+  # attr_reader :follower_ids
+  # attr_reader :follower_list
+  # attr_reader :profile
+  # attr_reader :income_friendship
 
-  user = JSON.parse(agent.get(url, [], nil, data).body)
+  def initialize(agent, token)
+    @agent = agent
+    @token = token
+
+    # @follower_ids = follower_ids()
+    # @follower_list = follower_list()
+    # @friend_ids = friend_ids()
+    # @profile = profile()
+    # @income_friendship = income_friendship()
+  end
+
+  # request follower's ids
+  def follower_ids(screen_name, cursor = -1, count = 5000)
+    url = 'https://api.twitter.com/1.1/followers/ids.json?cursor=' + cursor.to_s + '&screen_name=' + screen_name.to_s + '&count=' + count.to_s
+    request(@agent, @token, url)
+  end
+
+  # request followers' list
+  def follower_list(screen_name, cursor = -1, skip_status = true, include_user_entities = false)
+    url = 'https://api.twitter.com/1.1/followers/list.json?cursor=' + cursor.to_s + '&screen_name=' + screen_name.to_s  + '&skip_status=' + skip_status.to_s  + '&include_user_entities=' + include_user_entities.to_s 
+    request(@agent, @token, url)
+  end
+
+  # request friend's ids
+  def friend_ids(screen_name, cursor = -1, count = 5000)
+    url = 'https://api.twitter.com/1.1/friends/ids.json?cursor=' + cursor.to_s + '&screen_name=' + screen_name.to_s + '&count=' + count.to_s
+    p @token
+    request(@agent, @token, url)
+  end
+
+  # request friend's list
+  def friend_list(screen_name, cursor = -1, skip_status = true, include_user_entities = false)
+    url = 'https://api.twitter.com/1.1/friends/list.json?cursor=' + cursor.to_s + '&screen_name=' + screen_name.to_s  + '&skip_status=' + skip_status.to_s  + '&include_user_entities=' + include_user_entities.to_s 
+    request(@agent, @token, url)
+  end
+
+  # request income friendship
+  # TODO
+  # def income_friendship
+  #   url = 'https://api.twitter.com/1.1/friendships/incoming.json'
+  #   request(@agent, @token, url)
+  # end
+
+  # request user profile 
+  def profile(screen_name)
+    url = 'https://api.twitter.com/1.1/users/show/' + screen_name + '.json'
+    request(@agent, @token, url)
+  end
 end
