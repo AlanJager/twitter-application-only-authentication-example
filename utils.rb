20181c641d1fd511ac02e7d9ecc3af3289e8bd9d
AlanJager <873863981@qq.com>
2016-11-29 22:51:26 +0800

    add team func

--- utils.rb
+++ utils.rb
@@ -1,13 +1,27 @@
-def request_timeline(agent, token)
-  url = 'https://api.twitter.com/1.1/statuses/user_timeline.json?count=100&screen_name=twitterapi'
-  settings = YAML.load_file('settings.yml')
 
-  data = {
-    'Host' => 'api.twitter.com',
-    'User-Agent' => settings['user_agent'],
-    'Authorization' => "Bearer " + token,
-    'Accept-Encoding' => 'gzip'
-  }
+class Utils
+  attr_reader :agent
+  attr_reader :token
 
-  timelines = JSON.parse(agent.get(url, [], nil, data).body)
+  attr_reader :apis
+
+  def initialize(agent, token)
+    @agent = agent
+    @token = token
+
+    @apis = [
+      'request_timeline',
+      'list'
+    ]
+  end
+
+  def list(screen_name)
+    url = 'https://api.twitter.com/1.1/lists/list.json?screen_name=' + screen_name
+    request(@agent, @token, url)
+  end
+
+  def members(screen_name, slug = '', owner_screen_name = 'twitterapi', cursor = -1)
+    url = 'https://api.twitter.com/1.1/lists/members.json?slug=' + slug.to_s + '&owner_screen_name=' + owner_screen_name.to_s + '&cursor=' + cursor.to_s
+    request(@agent, @token, url)
+  end
 end
