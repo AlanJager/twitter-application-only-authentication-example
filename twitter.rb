1fff2402039ea3f0b5ec5b9542765d6cc87b6fb9
AlanJager <873863981@qq.com>
2016-11-29 22:49:45 +0800

    Twitter class for invoke

--- twitter.rb
+++ twitter.rb
@@ -1,23 +1,26 @@
-require 'mechanize'
 require 'json'
 require 'base64'
 require 'yaml'
 require_relative 'request'
 require_relative 'auth'
+require_relative 'search'
 require_relative 'user'
 require_relative 'utils'
 
-agent = Mechanize.new()
+class Twitter
+  attr_reader :agent
+  attr_reader :token
+  attr_reader :search
+  attr_reader :user
+  attr_reader :utils
+  
 
-token = auth(agent)
+  def initialize(agent, token)
+    @agent = agent
+    @token = token
 
-# request timelines
-# request_timeline(agent, token)
-
-# request user
-# request_user(agent, token, 'anju_inami')
-
-# request user following
-user = User.new(agent, token)
-p user.friend_ids('anju_inami')
-
+    @search = Search.new(agent, token)
+    @user = User.new(agent, token)
+    @utils = Utils.new(agent, token)
+  end
+end
